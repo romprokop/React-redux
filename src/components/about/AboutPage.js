9882b6d6ba653c56a9a17a8b68d68bb3a7304bde
romprokop <romekprokop@gmail.com>
2018-09-20 18:20:33 +0300

    .

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This app uses React, Redux and Reacr router in ES6</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
